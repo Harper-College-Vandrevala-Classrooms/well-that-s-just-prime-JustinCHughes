2610d5bdb7a9f0f8b67b561b8f9a427f5011c3a9
JustinCHughes <hcjustin713@gmail.com>
2024-08-26 22:46:57 -0500

    First iteration of program is done. Have not compiled or bug checked yet. Will check tomorrow

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -1,25 +1,47 @@
 package com.csc;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Stack;
 public class Factorizer {
 
   // You should implement your solution here.
   // Feel free to delete this example method when you implement your solution.
   public String primeListGeneration(int primeFactors)
   {
+    // As per instructions, program returns null if entry is equal to or less than 1
     if(primeFactors <= 1)
     {
       return null;
     }
-    List<Integer> primeList = new ArrayList<>();
+    // Create a stack to store the prime divisors
+    Stack<Integer> primeStack = new Stack<>();
 
+    // Iterates over possible divisors of primeFactors argument
     for(int divisor = 2; divisor <= primeFactors; divisor++)
     {
+      // Calls isPrime function below to check if divisor is a prime number
       if(isPrime(divisor))
       {
-        while(primeFactors % divisor)
+        // Continues dividing and adding to primeStack as long as primeFactors is still
+        // divisible with no remainder and primeFactors is still > 0
+        // sets primeFactors to divided amount then adds it to the stack
+        while(primeFactors % divisor == 0 && primeFactors > 0)
+        {
+          primeFactors = primeFactors / divisor;
+          primeStack.add(divisor);
+        }
+      }
+      // Checks if primeFactors is now 0 and then begins to build returnString
+      if (primeFactors == 0)
+      {
+        String returnString = Integer.toString(primeStack.pop());
+        while(!primeStack.isEmpty())
+        {
+          returnString = returnString + "x" + Integer.toString(primeStack.pop());
+        }
+        return returnString;
       }
     }
+    // primeFactors was unable to get to 0 and thus this integer is not "Prime"
+    return null;
   }
 
   // Checks if the divisor to be used is a prime number
